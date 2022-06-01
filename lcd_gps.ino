0c1906e8185527ac182a0e263570c7f55b566e2b
gratingBoy <48827595+gratingBoy@users.noreply.github.com>
2022-06-01 23:27:31 +0900

    起動音再生タイミング修正 受信時ビープ音追加
    起動音の再生がうまくいかなかったため、再生タイミングを修正した。
    電文受信時にビープ音を再生するように処理を追加した。

--- lcd_gps.ino
+++ lcd_gps.ino
@@ -5,20 +5,21 @@ LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(GPS_TX, GPS_RX);
 const int serialTime = 200;
+static int playFlag = OFF; // ビープ再生フラグ
 
 //********** arduino セットアップ **********
 void setup()
 {
+    play98();                     // 起動音再生
     lcd.begin(MAX_ROW, MAX_COL);  // LCD通信開始
     lcd.clear();                  // LCD画面クリア
     gpsSerial.begin(9600);        // GPS通信開始
     lcd.setCursor(0, 0);          // LCD カーソル移動 (1行目)
     lcd.print("GPS Multi Meter"); // 初期表示 1
     lcd.setCursor(0, 1);          // LCD カーソル移動 (2行目)
-    lcd.print("Version 0.2");     // 初期表示 2
-    play98();                     // 起動音再生
+    lcd.print("Version 0.03");    // 初期表示 2
     delay(INIT_WAIT);             // 待ち
-    lcd.clear();
+    lcd.clear();                  // LCD画面クリア
 } // end setup
 
 //********** メイン **********
@@ -39,21 +40,31 @@ void loop()
 
     if (gps.satellites.value() > 0) // 衛星から電文を受け取っているなら
     {
+        // 電文受信時にビープ再生を行う
+        if (playFlag == OFF) // 再生フラグOFFか
+        {
+            tone(SPEAKER_PIN, RECEIVE_TONE_LOW);  // 高音再生
+            delay(SOUND_DELAY);                   // 待ち
+            tone(SPEAKER_PIN, RECEIVE_TONE_HIGT); // 低音再生
+            delay(SOUND_DELAY);                   // 待ち
+            noTone(SPEAKER_PIN);                  // 再生停止
+            playFlag = ON;                        // 受信時ビープ再生フラグ ON
+        }
         if (gps.satellites.value() >= MINIMUM_SAT) // 3次元計測が可能なら
         {
-            if ((gps.time.hour() + 9) > 23) // 時差補正時に24時以上になるか
+            if ((gps.time.hour() + JST_OFFSET) > HOUR_23) // 時差補正時に24時以上になるか
             {
-                sprintf(str, "%02d:%02d ALT:%5dm",   // 時刻と標高表示
-                        gps.time.hour() + 9 - 24,    // GPSから時を取得(GMT->JST変換)
-                        gps.time.minute(),           // GPSから分を取得
-                        (int)gps.altitude.meters()); // GPSから標高を取得
+                sprintf(str, "%02d:%02d ALT:%5dm",              // 時刻と標高表示
+                        gps.time.hour() + JST_OFFSET - HOUR_24, // GPSから時を取得(GMT->JST変換)
+                        gps.time.minute(),                      // GPSから分を取得
+                        (int)gps.altitude.meters());            // GPSから標高を取得
             }
             else
             {
-                sprintf(str, "%02d:%02d ALT:%5dm",   // 時刻と標高表示
-                        gps.time.hour() + 9,         // GPSから時を取得(GMT->JST変換)
-                        gps.time.minute(),           // GPSから分を取得
-                        (int)gps.altitude.meters()); // GPSから標高を取得
+                sprintf(str, "%02d:%02d ALT:%5dm",    // 時刻と標高表示
+                        gps.time.hour() + JST_OFFSET, // GPSから時を取得(GMT->JST変換)
+                        gps.time.minute(),            // GPSから分を取得
+                        (int)gps.altitude.meters());  // GPSから標高を取得
             }
         }
         else // 3次元計測不可能なら
@@ -62,9 +73,9 @@ void loop()
                     gps.time.hour() + 9,         // GPSから時を取得(GMT->JST変換)
                     gps.time.minute());          // GPSから分を取得
         }
-        lcd.setCursor(0, 0); // カーソルを始点へ移動(1行目)
-        lcd.print(str);      // LCDにデータ表示
 
+        lcd.setCursor(0, 0);            // カーソルを始点へ移動(1行目)
+        lcd.print(str);                 // LCDにデータ表示
         lcd.setCursor(0, 1);            // カーソルを2行目に移動
         sprintf(str, "SPEED:%4dkm/h  ", // 速度表示
                 (int)gps.speed.kmph()); // GPSから速度を取得
@@ -76,6 +87,7 @@ void loop()
         lcd.print("Searching Signal"); // 1行目メッセージ表示
         lcd.setCursor(0, 1);           // カーソルを2行目へ移動
         lcd.print("Just a moment..."); // 2行目メッセージ表示
+        playFlag = OFF;                // 受信時ビープ 再生フラグ OFF
     }
 }
 
@@ -87,5 +99,4 @@ static void play98()
     tone(SPEAKER_PIN, TONE_LOW);  // 低音再生
     delay(SOUND_DELAY);           // 待ち
     noTone(SPEAKER_PIN);          // 再生停止
-    delay(SOUND_DELAY);           // 待ち
 }
