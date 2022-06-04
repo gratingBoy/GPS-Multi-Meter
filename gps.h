9ea0a298dda3084fd80a9481384f5e08597ac56a
gratingBoy <48827595+gratingBoy@users.noreply.github.com>
2022-06-04 22:56:41 +0900

    電文 未受信時ビープ再生処理の追加に伴う回収
    プロトタイプ宣言の追加
    GPSモジュールとの通信速度のdefine化など

--- gps.h
+++ gps.h
@@ -5,31 +5,36 @@
 #include <string.h>
 
 //********** define **********
-#define LCD_RS 2               // LCD RS Pin
-#define LCD_EN 3               // LCD Enable Pin
-#define LCD_D4 4               // LCD Data Bus 1
-#define LCD_D5 5               // LCD Data Bus 2
-#define LCD_D6 6               // LCD Data Bus 3
-#define LCD_D7 7               // LCD Data Bus 4
-#define SPEAKER_PIN 12         // スピーカー接続ピン
-#define GPS_TX 10              // GPS通信PIN(TX)
-#define GPS_RX 11              // GPS通信PIN(RX)
-#define TONE_HIGT 2000         // 高音周波数
-#define TONE_LOW 1000          // 低音周波数
-#define SOUND_DELAY 100        // 待ち時間(ms)
-#define MAX_ROW 16             // 最大文字数
-#define MAX_COL 2              // 最大行数
-#define INIT_WAIT 5000         // 初期表示待ち時間
-#define MINIMUM_SAT 4          // 3次元計測に必要な最低衛星数
-#define DISP_DELAY 200         // メッセージ表示ディレイ
-#define JST_OFFSET 9           // GMTとJSTとの時差
-#define HOUR_23 23             // 補正時刻
-#define HOUR_24 24             // 補正時刻
-#define ON 1                   // フラグON
-#define OFF 0                  // フラグOFF
-#define RECEIVE_TONE_HIGT 1500 //電文受診時ビープ 低音
-#define RECEIVE_TONE_LOW 2000  //電文受診時ビープ 高音
-#define MAIN_DELAY 200         //メインループ待ち時間
+#define LCD_RS 2                // LCD RS Pin
+#define LCD_EN 3                // LCD Enable Pin
+#define LCD_D4 4                // LCD Data Bus 1
+#define LCD_D5 5                // LCD Data Bus 2
+#define LCD_D6 6                // LCD Data Bus 3
+#define LCD_D7 7                // LCD Data Bus 4
+#define SPEAKER_PIN 12          // スピーカー接続ピン
+#define GPS_TX 10               // GPS通信PIN(TX)
+#define GPS_RX 11               // GPS通信PIN(RX)
+#define TONE_HIGT 2000          // 高音周波数
+#define TONE_LOW 1000           // 低音周波数
+#define SOUND_DELAY 100         // 待ち時間(ms)
+#define MAX_ROW 16              // 最大文字数
+#define MAX_COL 2               // 最大行数
+#define INIT_WAIT 5000          // 初期表示待ち時間
+#define MINIMUM_SAT 4           // 3次元計測に必要な最低衛星数
+#define DISP_DELAY 200          // メッセージ表示ディレイ
+#define JST_OFFSET 9            // GMTとJSTとの時差
+#define HOUR_23 23              // 補正時刻
+#define HOUR_24 24              // 補正時刻
+#define ON 1                    // フラグON
+#define OFF 0                   // フラグOFF
+#define RECEIVE_TONE_HIGT 1500  //電文受診時ビープ 低音
+#define RECEIVE_TONE_LOW 2000   //電文受診時ビープ 高音
+#define MAIN_DELAY 100          //メインループ待ち時間
+#define SEARCH_SIGNAL_TONE 400  // 信号未受信時ビープ音
+#define SEARCH_SIGNAL_DELAY 500 // 信号未受信時ビープ音ディレイ
+#define GPS_SERIAL 9600         // GPSモジュールとの通信速度
 
 //********** prototype **********
-static void play98();
+static void play98();              // 起動音ビープ再生
+static void signalReceivedBeep();  // 電文未信時ビープ再生
+static void SearchingSignalBeep(); // 電文未受信時ビープ再生
