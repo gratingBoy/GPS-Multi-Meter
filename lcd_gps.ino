c02eb570a2e509aa0b63b854513aeb775f2ca71f
gratingBoy <48827595+gratingBoy@users.noreply.github.com>
2022-06-04 22:55:27 +0900

    電文 未受信時ビープ音の追加など
    電文未受信時にビープ音を再生するようにした。
    受信時ビープ音再生処理を関数化した。

--- lcd_gps.ino
+++ lcd_gps.ino
@@ -5,7 +5,8 @@ LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(GPS_TX, GPS_RX);
 const int serialTime = 200;
-static int playFlag = OFF; // ビープ再生フラグ
+static int playFlag = OFF;            // 電文受信時ビープ再生フラグ
+static int SearchingSignalFlag = OFF; // 電文未受信時ビープ再生フラグ
 
 //********** arduino セットアップ **********
 void setup()
@@ -13,11 +14,11 @@ void setup()
     play98();                     // 起動音再生
     lcd.begin(MAX_ROW, MAX_COL);  // LCD通信開始
     lcd.clear();                  // LCD画面クリア
-    gpsSerial.begin(9600);      // GPS通信開始
+    gpsSerial.begin(GPS_SERIAL);  // GPS通信開始
     lcd.setCursor(0, 0);          // LCD カーソル移動 (1行目)
     lcd.print("GPS Multi Meter"); // 初期表示 1
     lcd.setCursor(0, 1);          // LCD カーソル移動 (2行目)
-    lcd.print("Version 0.03");    // 初期表示 2
+    lcd.print("Version 0.04");    // 初期表示 2
     delay(INIT_WAIT);             // 待ち
     lcd.clear();                  // LCD画面クリア
 } // end setup
@@ -40,16 +41,7 @@ void loop()
 
     if (gps.satellites.value() > 0) // 衛星から電文を受け取っているなら
     {
-        // 電文受信時にビープ再生を行う
-        if (playFlag == OFF) // 再生フラグOFFか
-        {
-            tone(SPEAKER_PIN, RECEIVE_TONE_LOW);  // 高音再生
-            delay(SOUND_DELAY);                   // 待ち
-            tone(SPEAKER_PIN, RECEIVE_TONE_HIGT); // 低音再生
-            delay(SOUND_DELAY);                   // 待ち
-            noTone(SPEAKER_PIN);                  // 再生停止
-            playFlag = ON;                        // 受信時ビープ再生フラグ ON
-        }
+        signalReceivedBeep();                      // 電文受信時にビープ再生を行う
         if (gps.satellites.value() >= MINIMUM_SAT) // 3次元計測が可能なら
         {
             if ((gps.time.hour() + JST_OFFSET) > HOUR_23) // 時差補正時に24時以上になるか
@@ -80,9 +72,11 @@ void loop()
         sprintf(str, "SPEED:%4dkm/h  ", // 速度表示
                 (int)gps.speed.kmph()); // GPSから速度を取得
         lcd.print(str);                 // LCDにデータ表示
+        SearchingSignalFlag = OFF;      // 未受信時ビープ再生フラグ ON
     }
     else // 測位無効の場合
     {
+        SearchingSignalBeep();         // 未受信時ビープ再生
         lcd.setCursor(0, 0);           // カーソルを始点へ移動
         lcd.print("Searching Signal"); // 1行目メッセージ表示
         lcd.setCursor(0, 1);           // カーソルを2行目へ移動
@@ -95,9 +89,37 @@ void loop()
 //********** 起動音再生 **********
 static void play98()
 {
-    tone(SPEAKER_PIN, TONE_HIGT); // 高音再生
-    delay(SOUND_DELAY);           // 待ち
-    tone(SPEAKER_PIN, TONE_LOW);  // 低音再生
-    delay(SOUND_DELAY);           // 待ち
-    noTone(SPEAKER_PIN);          // 再生停止
+    tone(SPEAKER_PIN, TONE_HIGT, SOUND_DELAY); // 高音再生
+    delay(SOUND_DELAY);                        // 待ち
+    tone(SPEAKER_PIN, TONE_LOW, SOUND_DELAY);  // 低音再生
+    delay(SOUND_DELAY);                        // 待ち
+    noTone(SPEAKER_PIN);                       // 再生停止
+}
+
+//********** 電文受信時ビープ再生 **********
+static void signalReceivedBeep()
+{
+    // 電文受信時にビープ再生を行う
+    if (playFlag == OFF) // 再生フラグOFFか
+    {
+        tone(SPEAKER_PIN, RECEIVE_TONE_LOW, SOUND_DELAY);  // 高音再生
+        delay(SOUND_DELAY);                                // 待ち
+        tone(SPEAKER_PIN, RECEIVE_TONE_HIGT, SOUND_DELAY); // 低音再生
+        delay(SOUND_DELAY);                                // 待ち
+        noTone(SPEAKER_PIN);                               // 再生停止
+        playFlag = ON;                                     // 受信時ビープ再生フラグ ON
+    }
+}
+
+//********** 電文未受信時ビープ再生 **********
+static void SearchingSignalBeep()
+{
+    // 電文未受信時にビープ再生を行う
+    if (SearchingSignalFlag == OFF) // 再生フラグOFFか
+    {
+        tone(SPEAKER_PIN, SEARCH_SIGNAL_TONE, SEARCH_SIGNAL_DELAY); // 高音再生
+        delay(SEARCH_SIGNAL_DELAY);                                 // 待ち
+        noTone(SPEAKER_PIN);                                        // 再生停止
+        SearchingSignalFlag = ON;                                   // 受信時ビープ再生フラグ ON
+    }
 }
